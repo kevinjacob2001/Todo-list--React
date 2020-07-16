c54c3f5929f635347e7b8bc58e740a6050f3e5a8
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-16 14:08:54 +0530

    completed the UI design. NOW IMPLEMENTING THE LOGIC PART

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -4,21 +4,43 @@ import { Component } from 'react';
 import './TodoApp.css';
 
 class TodoApp extends Component{
+state={
+    input:"",
+    items:[]
+}
+
+handleChange=(e)=>this.setState({input:e.target.value})
+
+
+storeItems=()=>{
+    const {input}=this.state;
+
+}
+
     render(){
+
+        const {input}=this.state;
+
         return(
             <div >
 <div className="todo-container">
     
 
-        <form className="input-section" onSubmit>
+        <form className="input-section" 
+        onSubmit={this.storeItems}>
+
         <h1>Todo App</h1>
-        <input type="text" placeholder="Enter items"/>
+        <input 
+        value={input}
+        onChange={this.handleChange}
+        type="text" 
+        placeholder="Enter items"/>
      
         </form>
 
-        <ul>
-        <li>Items</li>
-        <li>Items</li>
+        <ul >
+        <li>Items <i className="fas fa-trash-alt"></i></li>
+        
         </ul>
 </div>
 
