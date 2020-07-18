b851b3fb40d2888787ebf4bbbb2c7a9ebd79e38a
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-18 19:25:46 +0530

    Updates

---
+++ precache-manifest.23baacfac53fcf48d5c69e3279095db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a85b65b4aa0013daf278d8f8eaba2a",
+    "url": "/Todo-list--React/index.html"
+  },
+  {
+    "revision": "6ee6d82ea9f9cf560c58",
+    "url": "/Todo-list--React/static/css/main.ead8f847.chunk.css"
+  },
+  {
+    "revision": "f30e44cd054f985fe804",
+    "url": "/Todo-list--React/static/js/2.4e2b60d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list--React/static/js/2.4e2b60d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee6d82ea9f9cf560c58",
+    "url": "/Todo-list--React/static/js/main.934371e2.chunk.js"
+  },
+  {
+    "revision": "507514c6e3b2ce477cd4",
+    "url": "/Todo-list--React/static/js/runtime-main.2ecd51bb.js"
+  }
+]);
